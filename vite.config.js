7b69227270c1a8a7624bb61d67e84ba1440e102f
張豐展 <willy228@WillyMacBook-Pro.local>
2026-06-05 10:26:05 +0800

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  // 將原本的 '/anime-tracker/' 改為 './'
-  base: './', 
+  base: '/anime-tracker/',
   plugins: [react()],
-})
+});
